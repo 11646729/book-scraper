d842ace666ca7dd59a3d979e4212f1268d4f0e03
11646729 <bds6052@gmail.com>
2024-09-29 14:06:48 +0100

    Added working pagination code to harbourScraper.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import startBrowser from "./browser.js"
 import scraperController from "./pageController.js"
 
-// Url to browse
-let url = "http://books.toscrape.com"
-
 // Start the browser and create a browser instance
 let browserInstance = startBrowser()
 
 // Pass the browser instance to the scraper controller
-scraperController(browserInstance, url)
+scraperController(
+  browserInstance,
+  "https://www.belfast-harbour.co.uk/port/cruise-schedule/"
+)
